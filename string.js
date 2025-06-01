4800042f8a33c3e66b99317d20033f0bc0d66047
anik665 <97697517+anik665@users.noreply.github.com>
2025-06-01 12:09:33 +0600

    closer

--- string.js
+++ string.js
@@ -1,12 +1,30 @@
-function findRemoveStringIndex(str1, str2) {
-    let result = [];
-    for (let i = 0; i < str1.length; i++) {
-        let new_str1 = str1.slice(0, i) + str1.slice(i + 1);
-        if (new_str1 === str2) {
-            result.push(i)
-        }
+// function findRemoveStringIndex(str1, str2) {
+//     let result = [];
+//     for (let i = 0; i < str1.length; i++) {
+//         let new_str1 = str1.slice(0, i) + str1.slice(i + 1);
+//         if (new_str1 === str2) {
+//             result.push(i)
+//         }
 
+//     }
+//     return result;
+// }
+// console.log( findRemoveStringIndex("abdggge", "abdgge"))
+
+
+function findRemoveStringIndex(str1, str2) {
+    let i = 0, j = 0;
+    let result = [];
+    while (i < str1.length && j < str2.length) {
+        if (str1[i] === str2[j]) {
+            i++;
+            j++;
+        }
+        else {
+            result.push(i);
+            i++;
+        }
     }
     return result;
 }
-console.log( findRemoveStringIndex("abdggge", "abdgge"))
+console.log(findRemoveStringIndex("abdggge", "abdgge"))
